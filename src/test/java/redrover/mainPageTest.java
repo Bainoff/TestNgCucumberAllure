c169a7d953115cf37f13549c14ef4cb8857a337a
Alexey <bainoff@gmail.com>
2024-02-09 00:10:01 +0100

    allure tags and other stuff added

--- src/test/java/redrover/mainPageTest.java
+++ src/test/java/redrover/mainPageTest.java
@@ -1,6 +1,6 @@
 package redrover;
 
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,12 +8,17 @@ import runner.BaseTest;
 
 public class mainPageTest extends BaseTest {
 
+    @Epic("Test epic")
     @Story("Open main page")
+    @Link("https://redrover.school")
+    @Description("desc example2")
+    @Severity(SeverityLevel.NORMAL)
     @Test
     public void openRedRoverMainPageP(){
-
         getDriver().get("https://redrover.school");
         Assert.assertEquals(getDriver().getTitle(), "RedRover | Non-commercial it-school");
+        Allure.step("check main page title");
+        Allure.description("additional description inside test");
 
     }
 
@@ -26,11 +31,16 @@ public class mainPageTest extends BaseTest {
 
     }*/
 
-
+    @Epic("Test epic")
+    @Story("Open main page")
+    @Link("https://redrover.school")
+    @Severity(SeverityLevel.MINOR)
+    @Description("desc example2")
     @Test
     public void header1Present(){
         getDriver().get("https://redrover.school");
-
+        Allure.step("step header check");
+        Allure.description("additional description method");
         Assert.assertTrue(getDriver().findElement(By.xpath("//a[@href = '/#training']")).isEnabled());
 
 
